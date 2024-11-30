07d58cd8b88b5b22710bf5ab1da8d7e347377917
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-30 11:14:37 +0200

    feat: sign up method

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
+import { UsersService } from './users.service'
+import * as bcrypt from 'bcrypt'
+import { CreateUserDto } from 'src/DTOs/createUserDto'
+import { ApiTags, ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger'
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/signup')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: 'User successfully registered' })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  async addUser(@Body() createUserDto: CreateUserDto) {
+    const saltOrRounds = Number(process.env.SALT_OR_ROUNDS)
+    const hashedPassword = await bcrypt.hash(
+      createUserDto.password,
+      saltOrRounds,
+    )
+    const result = await this.usersService.insertUser(
+      createUserDto.username,
+      hashedPassword,
+    )
+    return {
+      msg: 'User successfully registered',
+      userId: result.id,
+      userName: result.username,
+    }
+  }
+}
