212e87f93a1ff19761ed6337b967b355e8df7eb2
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-30 10:47:53 +0200

    chore: users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_LOCAL_CONNECTION),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
