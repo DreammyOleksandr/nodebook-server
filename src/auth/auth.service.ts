035f275a72a4a2deaa125bef922c44756a393e2a
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-11-30 11:32:09 +0200

    feat: user validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotAcceptableException } from '@nestjs/common'
+import { UsersService } from 'src/users/users.service'
+import * as bcrypt from 'bcrypt'
 
 export interface User {
   email: string
@@ -6,4 +8,20 @@ export interface User {
 }
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.getUser(username)
+    const passwordValid = await bcrypt.compare(password, user.password)
+    if (!user) {
+      throw new NotAcceptableException('could not find the user')
+    }
+    if (user && passwordValid) {
+      return {
+        userId: user.id,
+        userName: user.username,
+      }
+    }
+    return null
+  }
+}
